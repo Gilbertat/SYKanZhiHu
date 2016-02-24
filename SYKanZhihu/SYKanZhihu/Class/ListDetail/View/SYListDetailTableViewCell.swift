837d6b760d159af960ebed2117810162f05db03a
shiyue <shiyue45457@126.com>
2016-02-24 15:33:45 +0800

    adjustment layout

--- SYKanZhihu/SYKanZhihu/Class/ListDetail/View/SYListDetailTableViewCell.swift
+++ SYKanZhihu/SYKanZhihu/Class/ListDetail/View/SYListDetailTableViewCell.swift
@@ -39,6 +39,26 @@ class SYListDetailTableViewCell: UITableViewCell {
         self.authorLabel.setTitle(model.authorname, forState: .Normal)
     }
     
+    override var frame:CGRect {
+        get {
+            return super.frame
+        }
+        set (newframe) {
+            var frame = newframe
+            frame.origin.x += 10
+            frame.size.width -= 2 * 10
+            super.frame = frame
+        }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let maskPath = UIBezierPath(roundedRect:self.bounds, byRoundingCorners:[.BottomLeft,.BottomRight], cornerRadii: CGSizeMake(5.0, 5.0))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = maskPath.CGPath
+        self.layer.mask = maskLayer
+    }
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- SYKanZhihu/SYKanZhihu/Class/ListDetail/View/SYListQuestionTableViewCell.swift
+++ SYKanZhihu/SYKanZhihu/Class/ListDetail/View/SYListQuestionTableViewCell.swift
@@ -12,5 +12,22 @@ class SYListQuestionTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
    
-    
+    override var frame:CGRect {
+        get {
+            return super.frame
+        }
+        set (newframe) {
+            var frame = newframe
+            frame.origin.x += 10
+            frame.size.width -= 2 * 10
+            super.frame = frame
+        }
+    }
+    override func drawRect(rect: CGRect) {
+        let maskPath = UIBezierPath(roundedRect:self.bounds, byRoundingCorners: [.TopLeft,.TopRight], cornerRadii: CGSizeMake(5.0, 5.0))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = maskPath.CGPath
+        self.layer.mask = maskLayer
+    }
 }
