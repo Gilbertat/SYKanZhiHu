9afb1f86cd71dfce3f2fc06c860f05693946874e
Gilbertat <shiyue45457@gmail.com>
2016-03-01 11:43:47 +0800

    layout SYTopUserTableViewCell

---
+++ SYKanZhihu/SYKanZhihu/Class/Ranking/View/SYTopUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SYTopUserTableViewCell.swift
+//  SYKanZhihu
+//
+//  Created by shiyue on 16/3/1.
+//  Copyright © 2016年 shiyue. All rights reserved.
+//
+
+import UIKit
+
+class SYTopUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
