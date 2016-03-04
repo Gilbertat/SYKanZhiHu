6895b6b61904ccf60c83c11b8607bfd1820b221d
Gilbertat <shiyue45457@gmail.com>
2016-03-04 10:33:20 +0800

    add Refresh Document

---
+++ SYKanZhihu/SYKanZhihu/Class/Manager/Refresh/RefreshBase.swift
@@ -0,0 +1,9 @@
+//
+//  RefreshBase.swift
+//  SYKanZhihu
+//
+//  Created by shiyue on 16/3/4.
+//  Copyright © 2016年 shiyue. All rights reserved.
+//
+
+import Foundation
---
+++ SYKanZhihu/SYKanZhihu/Class/Manager/Refresh/RefreshConst.swift
@@ -0,0 +1,24 @@
+//
+//  RefreshConst.swift
+//  SYKanZhihu
+//
+//  Created by shiyue on 16/3/4.
+//  Copyright © 2016年 shiyue. All rights reserved.
+//
+
+import Foundation
+
+let RefreshViewHeight:Float = 64.0 //refreshView高度
+let RefreshAnimationDuration:NSTimeInterval = 0.3 //刷新持续时间
+
+let RefreshFooterToRefresh:String = "上拉加载更多数据"
+let RefreshFooterRelease:String = "松开立即加载更多数据"
+let RefreshFooterRefreshing:String = "正在加载数据..."
+
+let RefreshHeaderToRefresh:String = "下拉刷新更多数据"
+let RefreshHeaderRelease:String = "松开立即刷新更多数据"
+let RefreshHeaderRefreshing:String = "正在刷新中..."
+
+let RefreshHeaderTimeKey:String = "RefreshHeaderView"
+let RefreshContentOffSet:String = "contentOffset"
+let RefreshContentSize:String = "ContentSize"
